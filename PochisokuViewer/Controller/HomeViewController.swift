96c794f64a35d854d4e29463c3313dfdafa5f8aa
南 京兵 <k_minami@waku-2.com>
2018-02-16 19:02:19 +0900

    テーブルビューのセクション数を記事の数を参照するように変更

--- PochisokuViewer/Controller/HomeViewController.swift
+++ PochisokuViewer/Controller/HomeViewController.swift
@@ -43,7 +43,7 @@ class HomeViewController: UIViewController {
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return relatedArticleList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
